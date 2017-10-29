0178f85e7a0e5c8d0269888ba07178c2a9a3ab6e
ngowd001 <ngowd001@ucr.edu>
2017-10-29 00:33:58 -0700

    Nothing truely functional, just a start

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+bool exit = false;
+string command = "";
+
+
+while (!exit)
+	{
+		cout << "$";
+		cin >> command;
+		cout << endl;
+	}
+
+
+
+
+return 0;
+}
+
+
