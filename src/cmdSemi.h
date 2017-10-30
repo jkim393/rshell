8e9a8b0d1fd76a3a22034e0d6492babe2fec5720
ngowd001 <ngowd001@ucr.edu>
2017-10-30 16:13:00 -0700

    added Semi Class

---
+++ src/cmdSemi.h
@@ -0,0 +1,9 @@
+#ifndef _CMDSEMI_H_
+#define _CMDSEMI_H_
+	class cmdSemi : public cmdConn {
+		public:
+			cmdSemi() : cmdConn() {};
+			cmdSemi(cmdBase* left, cmdBase* right) : cmdConn(left, right) {};
+			bool evaluate(); 			
+
+#endif
