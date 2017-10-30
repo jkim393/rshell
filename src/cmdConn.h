06c6ca124273764f45519c004991a8bc0b3c0a5c
ngowd001 <ngowd001@ucr.edu>
2017-10-30 15:45:54 -0700

    created cmdConnector class

---
+++ src/cmdConn.h
@@ -0,0 +1,20 @@
+#ifndef _CMDCONN_H_
+#define _CMDCONN_H_
+
+
+class cmdConnector : public cmdBase {
+			protected:
+								cmdBase* left;
+								cmdBase* right;
+
+			public:	
+								cmdConnector() : cmdBase(){};
+								cmdConnector(cmdBase* l, cmdBase* r): left (l), right(r){};
+								
+								cmdBase* get_left() {return left;};
+								cmdBase* get_right() {return right;};
+								virtual bool evaluate() = 0;
+
+};
+
+#endif
