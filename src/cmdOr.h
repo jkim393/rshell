18bf4f06b4fad87608d7ded7de992575b97b31a9
ngowd001 <ngowd001@ucr.edu>
2017-10-31 08:35:14 -0700

    fixed spacing of class

--- src/cmdOr.h
+++ src/cmdOr.h
@@ -1,11 +1,11 @@
 #ifndef _CMDOR_H_
 #define _CMDOR_H_
 
-		class cmdOr : public cmdConn {
-					public: 
-								cmdOr(): cmdConn() {};
-								cmdOr(cmdBase* left, cmdBase* right) : cmdConn(left, right) {};
-								bool evaluate();
+	class cmdOr : public cmdConn {
+		public: 
+			1cmdOr(): cmdConn() {};
+			cmdOr(cmdBase* left, cmdBase* right) : cmdConn(left, right) {};
+			bool evaluate();
 
 
 #endif
