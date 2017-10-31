cb1d5eefee4e2790681cc4664580bdc2d97c8426
ngowd001 <ngowd001@ucr.edu>
2017-10-31 08:42:04 -0700

    FIX SPACING OF AND

--- src/cmdAnd.h
+++ src/cmdAnd.h
@@ -2,12 +2,10 @@
 #define _CMDAND_H_
 
 	class cmdAnd : public cmdConn {
-				public:
-							cmdAnd(): cmdConn() {};
-							cmdAnd(cmdBase* left, cmdBase* right) : cmdConn(left, right) {};
-							bool evaluate();
-					};
-
-
+		public:
+			cmdAnd(): cmdConn() {};
+			cmdAnd(cmdBase* left, cmdBase* right) : cmdConn(left, right) {};
+			bool evaluate();
+	};
 
 #endif
