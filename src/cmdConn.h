d10ba21b75601a1ca8e24645948da952f5ee073e
ngowd001 <ngowd001@ucr.edu>
2017-10-31 08:39:56 -0700

    Fixed spacing of class

--- src/cmdConn.h
+++ src/cmdConn.h
@@ -2,14 +2,14 @@
 #define _CMDCONN_H_
 
 
-class cmdConnector : public cmdBase {
+class cmdConn : public cmdBase {
 	protected:
 		cmdBase* left;
 		cmdBase* right;
 
 	public:	
-		cmdConnector() : cmdBase(){};
-		cmdConnector(cmdBase* l, cmdBase* r): left (l), right(r){};
+		cmdConn() : cmdBase(){};
+		cmdConn(cmdBase* l, cmdBase* r): left (l), right(r){};
 		cmdBase* get_left() {return left;};
 		cmdBase* get_right() {return right;};
 		virtual bool evaluate() = 0;
