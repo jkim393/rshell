c1958e4f77679201151ae8f278ac4dd4636c5475
ngowd001 <ngowd001@ucr.edu>
2017-11-01 17:47:30 -0700

    Test Nikhil Branch

--- main.cpp
+++ main.cpp
@@ -2,27 +2,20 @@
 
 using namespace std;
 
+
+void cmdPrompt()
+{
+	cout << "Fix Me" << endl;
+	return;
+}
+
+
+
+
 int main()
 {
 
-bool exit = false;
-string executable = "";
-string argumentList = "";
-string connector = "";
-
-// I did not really add anything, just wanted a basic skeleton?
-// very naive implementation
-while (!exit)
-	{
-		cout << "$";
-		cin >> executable;
-		cin >> argumentList;
-		cin >> connector;
-
-		exit = true;
-	}
-
-	cout << "\n" << executable << "\n" << argumentList << "\n" << connector << endl;
+cmdPrompt();
 
 return 0;
 }
