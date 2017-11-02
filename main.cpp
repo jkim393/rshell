0e823737f1c5cae6b285eaf7cfd43dd5c91dabb9
ngowd001 <ngowd001@ucr.edu>
2017-11-01 17:44:15 -0700

    Test github add

--- main.cpp
+++ main.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
+
+void cmdPrompt()
+{
+	cout << "FINISH ME" << endl;
+}
+	
 
 int main()
 {
 
-bool exit = false;
-string executable = "";
-string argumentList = "";
-string connector = "";
+cmdPrompt();
 
-// I did not really add anything, just wanted a basic skeleton?
-// very naive implementation
-while (!exit)
-	{
-		cout << "$";
-		cin >> executable;
-		cin >> argumentList;
-		cin >> connector;
-
-		exit = true;
-	}
-
-	cout << "\n" << executable << "\n" << argumentList << "\n" << connector << endl;
 
 return 0;
 }
