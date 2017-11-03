f2778a8a733b6b48a42e49a43e06ec4c329eb868
ngowd001 <ngowd001@ucr.edu>
2017-11-03 01:52:59 -0700

    Finished creating 2d array

--- main.cpp
+++ main.cpp
@@ -10,7 +10,28 @@ iteration. if a connector is detected i create a new row and iterate. for semi c
 and add it to a new row and continue iterating. 
 */
 
-void cmdPrompt()
+
+
+void removeSemis(vector< vector<char*> > &finishedVect)
+	{
+		int i = 0;
+		int x = 0;
+		char* last_lett;
+		
+		for (i = 0; i < finishedVect.size(); ++i)
+				{
+					for (x = 0; x < finishedVect.at(i).size(); ++x)
+						{
+							last_lett = &(finishedVect.at(i).at(x)[strlen(finishedVect.at(i).at(x))-1]);
+							if (*(last_lett) == ';')
+								{
+									finishedVect.at(i).at(x)[strlen(finishedVect.at(i).at(x)) - 1] = 0;
+								}
+							}
+					}				
+		}
+
+void cmdPrompt(vector< vector<char*> > &cmdVector)
 {
 
 	string userInput = "";	//test string
@@ -21,57 +42,110 @@ void cmdPrompt()
 	char* pch;	//used to iterate through list
 	pch = strtok(str, " ");
 	char* last_letter = &(pch[strlen(pch)-1]);
-	vector< vector<char*> > cmdVector;
 	char* orsign = new char('|');		//orsign pointer pointing to or sign
 	char* andsign = new char('&');		//andsign pointer pointing to and sign
 	bool connectorTrue = false; // checks if previous parse was a connector 
 	vector<char*> andSign;
-	andSign.push_back(andSign);
+	andSign.push_back(andsign);
+	vector<char*> orSign;
+	orSign.push_back(orsign);
 	while (pch != NULL)
 	{
-
-		if (*(last_letter) == ';')							// HANDLES THE SEMI COLON
-			{ 
-				cmdVector.back().push_back(pch);
-				connectorTrue = true; // previous was a connector
-			}  
-   else if (*(last_letter) == '|')					//HANDLES THE ||
+		if (connectorTrue)
 			{
-				cmdVector.push_back.push_back(orsign);
+				vector<char*> tempVect;
+				tempVect.push_back(pch);
+				cmdVector.push_back(tempVect);
+					if (*(last_letter) == '|' || *(last_letter) == '&' || *(last_letter) == ';')
+						{
+							connectorTrue = true;
+						}
+						else
+						{
+							connectorTrue = false;
+						}
+					}
+					
+		else if(*(last_letter) == ';')							// HANDLES THE SEMI COLON
+			{ 
+				if(cmdVector.size() == 0)
+					{
+						vector<char*> tempVect;
+						tempVect.push_back(pch);
+			    	cmdVector.push_back(tempVect);
+						connectorTrue = true;
+					}
+				else
+					{
+        	 cmdVector.back().push_back(pch);
+        	 connectorTrue = true; // previous was a connector
+					}
+			}  
+  	else if (*(last_letter) == '|')					//HANDLES THE ||
+			{
+				cmdVector.push_back(orSign);
 				connectorTrue = true; //previous was a connector
 			}
 		else if (*(last_letter) == '&')
 			{
-				cmdVector.push_back(andsign);
+				cmdVector.push_back(andSign);
 				connectorTrue = true; //previous was a connector
-			}
-		else if (connectorTrue)								//HANDLES THE &&
-			{
-				cmdVector.push_back.push_back(pch); 
-				connectorTrue = false;
 			}
 		else 
 		{
-			cmdVector.back().push_back(pch);
+			if(cmdVector.size() == 0)
+			{
+				vector<char*> tempVect;
+				tempVect.push_back(pch);
+				cmdVector.push_back(tempVect);
+			}
+			else
+		  {
+				cmdVector.back().push_back(pch);
+			}
 		}
-			last_letter = &(pch[strlen(pch)-1]);
-			pch = strtok(NULL, " ");
+		  cout << "+ good" << endl;
+		  pch = strtok(NULL," ");
+			if (pch != NULL)
+			{
+			  last_letter = &(pch[strlen(pch)-1]);
+			}
 	}
 	
-	//all arguments are in cmdVector, we now will add the connectors
-
+	int a = 0;
+	int b = 0;
+	for (a = 0; a < cmdVector.size(); ++a)
+		{
+			for (b = 0; b < cmdVector.at(a).size(); ++b)
+				{
+					cout << cmdVector.at(a).at(b) << ' ';
+				}
+			cout << endl;
+		}
 	
-	
-
+	removeSemis(cmdVector); // this command removes the semis before returning
 	return;
 }
 
 
 int main()
 {
-
-	cmdPrompt();
+	vector< vector<char*> > execVect;   // this is a vector that holds the 2d array of commands + connectors (no semis)
+	cmdPrompt(execVect); // this is what creates the array
 	
+  int a = 0;
+	int b = 0;
+	for (a = 0; a < execVect.size(); ++a)
+			{
+				for (b = 0; b < execVect.at(a).size(); ++b)
+					{
+						cout << execVect.at(a).at(b) << ' ';
+					}
+			cout << endl;
+			}
+	cout << execVect.size() << endl;
+	cout << execVect.at(1).size() << endl;
+  cout << execVect.at(3).size() << endl;
 
 return 0;
 }
