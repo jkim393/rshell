934c22b6ae5c9f686a16b92e4cbc1d57f35f39b0
Ji Kim <jkim393@ucr.edu>
2017-12-03 02:03:15 -0800

    <,>,>> working but not pipe

--- src/cmdExec.cpp
+++ src/cmdExec.cpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string.h>
+#include <fcntl.h>
 
 using namespace std;
 
@@ -71,6 +72,36 @@ bool cmdExec::runTest(vector<char*> testVector){
 
 }
 
+bool cmdExec::checkRedirection(vector<char*> testingVector){
+	char* testV;
+	vector<char*> testVect = testingVector;
+	bool trueRedirect = false;
+	for (int i = 0; i < testVect.size(); ++i)
+	{
+		testV = strchr(testVect.at(i), '<');
+		if (testV != NULL)
+		{
+			trueRedirect = true;
+			return trueRedirect;
+		}
+											
+		testV = strchr(testVect.at(i), '>');
+		if (testV != NULL)
+		{
+			trueRedirect = true;
+			return trueRedirect;
+		}
+											
+		testV = strchr(testVect.at(i), '|');
+		if (testV != NULL)
+		{
+			trueRedirect = true;
+			return trueRedirect;
+		}
+	
+	}		
+		return trueRedirect; //returns false	
+}
 
 bool cmdExec::evaluate(){
 	if (v.size() == 0) { //empty case
@@ -79,19 +110,151 @@ bool cmdExec::evaluate(){
 	else if ( strcmp(v.at(0), "test") == 0 || strcmp(v.at(0), "[") == 0 ) { //test case
 		return runTest(v);
 	}
-	else if (strcmp(v.at(0), "exit") == 0  ) {
+	else if (strcmp(v.at(0), "exit") == 0  ) { //test case without []
 		_exit(0);
 	}
+	
+	pid_t pid = fork();
+	if (pid == 0){ //child
+		
+		// if (call function that checks if there is >,<,>>, | returns true)
+  	// 		then call function that takes the thing and 
+ 		if (checkRedirection(v)) { 
+			//case of   <  >	
+			if (v.size() >= 5 && strcmp(v.at( v.size() - 2 ), ">") == 0 && strcmp(v.at( v.size() - 4 ), "<") == 0 )  {  
+				const char* pathname = (const char *)v.at(v.size() - 1); //get the path name
+				const char* pathname2= (const char *)v.at(v.size() - 3);
+				int out = open(pathname, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+				int in  = open(pathname2, O_RDONLY);
+					v.pop_back();
+					v.pop_back();
+					v.pop_back();
+					v.pop_back();
+				v.push_back(NULL); 			//adding Null for the execvp needs the Null at the end
+				char **command = &v[0]; //converting vector into array
+				//const	char* e = "exit"; //exit function
+				dup2(in, 0);
+				dup2(out,1);
+				close(in);
+				close(out);
 
+				if (execvp(command[0],command) == -1) {
+					perror ("exec");
+					return false;        //return before close might cause error
+				}
+
+			}
+			//case of  >
+			else if (strcmp(v.at( v.size() - 2 ), ">") == 0 ) {  
+				const char* pathname = (const char *)v.at(v.size() - 1); //get the path name
+				int out = open(pathname, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+				
+					v.pop_back();
+					v.pop_back();
+				
+				v.push_back(NULL); 			//adding Null for the execvp needs the Null at the end
+				char **command = &v[0]; //converting vector into array
+				//const	char* e = "exit"; //exit function
+				dup2(out,1);
+				close(out);
+
+				if (execvp(command[0],command) == -1) {
+					perror ("exec");
+					return false;        //return before close might cause error
+				}
+			}
+			//case of  <
+			else if (strcmp(v.at( v.size() - 2 ), "<") == 0 ) {  
+				const char* pathname = (const char *)v.at(v.size() - 1); //get the path name
+				int in = open(pathname, O_RDONLY);
+				
+					v.pop_back();
+					v.pop_back();
+				
+				v.push_back(NULL); 			//adding Null for the execvp needs the Null at the end
+				char **command = &v[0]; //converting vector into array
+				//const	char* e = "exit"; //exit function
+				dup2(in,0);
+				close(in);
+
+				if (execvp(command[0],command) == -1) {
+					perror ("exec");
+					return false;        //return before close might cause error
+				}
+			}
+			else if (v.size() >= 5 && strcmp(v.at( v.size() - 2 ), ">>") == 0 && strcmp(v.at( v.size() - 4 ), "<") == 0 )  {  
+				const char* pathname = (const char *)v.at(v.size() - 1); //get the path name
+				const char* pathname2= (const char *)v.at(v.size() - 3);
+				int out = open(pathname, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+				int in  = open(pathname2, O_RDONLY);
+					v.pop_back();
+					v.pop_back();
+					v.pop_back();
+					v.pop_back();
+				v.push_back(NULL); 			//adding Null for the execvp needs the Null at the end
+				char **command = &v[0]; //converting vector into array
+				//const	char* e = "exit"; //exit function
+				dup2(in, 0);
+				dup2(out,1);
+				close(in);
+				close(out);
+
+				if (execvp(command[0],command) == -1) {
+					perror ("exec");
+					return false;        //return before close might cause error
+				}
+
+			}
+			//case of  >>
+ 			else if (strcmp(v.at( v.size() - 2 ), ">>") == 0 ) {  
+				const char* pathname = (const char *)v.at(v.size() - 1); //get the path name
+				int out = open(pathname, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+				
+					v.pop_back();
+					v.pop_back();
+				
+				v.push_back(NULL); 			//adding Null for the execvp needs the Null at the end
+				char **command = &v[0]; //converting vector into array
+				//const	char* e = "exit"; //exit function
+				dup2(out,1);
+				close(out);
+
+				if (execvp(command[0],command) == -1) {
+					perror ("exec");
+					return false;        //return before close might cause error
+				}
+			}
+
+			//case of |
+			return true;
+		}
+		//another case
+		else {
+			v.push_back(NULL); 			//adding Null for the execvp needs the Null at the end
+			char **command = &v[0]; //converting vector into array
+			if (execvp(command[0],command) == -1) {
+				perror ("exec");
+				return false;
+			}
+		}
+	
+	}//end of child process
+
+	if (pid > 0) { //parent
+			if ( waitpid(0, NULL, 0) == -1 ) {
+				perror ("wait");
+			}
+	}
+
+	return true;
+}
+	/*
 	v.push_back(NULL); 			//adding Null for the execvp needs the Null at the end
 	char **command = &v[0]; //converting vector into array
 	//const	char* e = "exit"; //exit function
 	pid_t pid = fork();
 
 	if (pid == 0){ //child
-	//	if (command[0] == e){
-			//	exit(0);	
-		//}
 		
 		if (execvp(command[0],command) == -1) {
 			perror ("exec");
@@ -106,4 +269,4 @@ bool cmdExec::evaluate(){
 		}
 	}
 	return true;
-}
+}*/
